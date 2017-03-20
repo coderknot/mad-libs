1619878fd19578263545ebad4449f6fff58bcd8f
Chris Carr and Riley Watts <chris@>
2017-03-20 09:30:56 -0700

    create story template and add key values to model hash map

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,12 @@ public class App {
 
     get("/story", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("person1", request.queryParams("person1"));
+      model.put("person2", request.queryParams("person2"));
+      model.put("animal", request.queryParams("animal"));
+      model.put("exclamation", request.queryParams("exclamation"));
+      model.put("verb", request.queryParams("verb"));
+      model.put("noun", request.queryParams("noun"));
       model.put("template", "templates/story.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
